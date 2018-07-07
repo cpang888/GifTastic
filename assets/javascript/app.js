331b1897df563122e312748822198aa8eb0a9a04
Cindy Pang <cipang@coca-cola.com>
2018-07-07 09:24:01 -0400

    repush

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,28 @@
     
   function start() {
 
+
+    var queryURL = "https://jobs.search.gov/jobs/search.json?query=Technology+jobs+in+Atlanta,Georgia";
+    // var queryURL = "https://api.linkedin.com/v2/recommendedJobs?q=byMember";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      contentType: "application/json",
+      dataType: 'jsonp',
+      success: function(data) {
+        console.log("success");
+        console.log(data);
+      },
+      error: function(err) {
+        console.log('Error', err);
+      }
+    }).then(function(response) {
+      console.log("gov jobs");
+        var results = response.data;
+        console.log(response);
+    });
+
     // the current animal which was clicked
     var animal = "";
     var favoriteImg = "";
